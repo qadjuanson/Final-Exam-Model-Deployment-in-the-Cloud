b5cc20b1aea255eaf3612abd88d9be83b425512c
qadjuanson <qadjuanson@tip.edu.ph>
2025-05-22 18:19:45 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+# Load model
+model = tf.keras.models.load_model("weather_mobilenetv2.h5")
+class_names = ['Cloudy', 'Fog', 'Rain', 'Shine', 'Sunrise']
+
+st.title("Weather Image Classifier 🌦️")
+st.markdown("Upload an image and let the AI predict the weather condition.")
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert('RGB')
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = image.resize((224, 224))
+    img_array = np.array(img) / 255.0
+    img_array = np.expand_dims(img_array, axis=0)
+
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions)]
+
+    st.success(f"Prediction: **{predicted_class}**")
